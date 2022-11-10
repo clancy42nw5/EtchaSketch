73e4d37e5fcd0083d61ce9ab5dae519828ab3c31
Clancy Wilson <116132142+clancy42nw5@users.noreply.github.com>
2022-11-10 11:56:05 +1100

    Add trace functionality

--- javascript.js
+++ javascript.js
@@ -1,22 +1,30 @@
 const body = document.querySelector('body');
 
-for (let i=0; i < 4; i++) {
+function removeTransition(e) {
+    //if (e.propertyName !== 'transform') return;
+    this.classList.remove('activated');
+}
+
+let numberOfRows = 16
+let numberOfColumns = 16
+
+for (let i=0; i < numberOfRows; i++) {
     const newRow = document.createElement ('newRow');
     newRow.classList.add('row');
     body.appendChild(newRow);
-    for (let i=0; i < 4; i++) {
+    for (let i=0; i < numberOfColumns; i++) {
         const newColumn = document.createElement('newColumn');
         newColumn.classList.add('column');
         newRow.appendChild(newColumn);
-
-    }    
-}
-/*
-newColumn.addEventListener('mouseover', () => {
-    this.target.style.backgroundcolor = 'blue';
-});
-
-onmouseover = () => {
-
-};
-*/
+        newColumn.addEventListener('mouseover', () => {
+            newColumn.classList.add('activated');
+            newColumn.addEventListener('transitionend', removeTransition);
+        });
+        newColumn.addEventListener('mouseleave', () => {
+            
+        })
+        newColumn.addEventListener('mousedown', () => {
+            newColumn.classList.add('permanent');
+        })
+        }
+    }
