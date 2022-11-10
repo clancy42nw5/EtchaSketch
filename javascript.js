138ec0f537ab0d943587a4fdcbe19c133d8130d5
Clancy Wilson <116132142+clancy42nw5@users.noreply.github.com>
2022-11-10 14:58:18 +1100

    Add shrink and grow functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,59 @@
 const gridContainer = document.querySelector('.gridContainer');
 
-let numberOfRows = 100
-let numberOfColumns = 100
+let numberOfRows = 96;
 
+// minimum of 8, maximum of 144
+
+const lessPixels = document.querySelector('#lessPixels')
+const morePixels = document.querySelector('#morePixels')
+
+lessPixels.addEventListener('click', () => {
+    if (numberOfRows >= 16) {
+        deleteRows();
+        numberOfRows -= 8;
+        createRows();
+    }
+});
+
+morePixels.addEventListener('click', () => {
+    if (numberOfRows <= 144) {
+        deleteRows();
+        numberOfRows += 8;
+        createRows();
+    }
+});
+
+function createRows() {
+    for (let i=0; i < numberOfRows; i++) {
+        const newRow = document.createElement ('newRow');
+        newRow.classList.add('row');
+        gridContainer.appendChild(newRow);
+        for (let i=0; i < numberOfRows; i++) {
+            const newColumn = document.createElement('newColumn');
+            newColumn.classList.add('column');
+            newRow.appendChild(newColumn);
+            newColumn.addEventListener('mouseover', () => {
+                newColumn.classList.add('activated');
+            });
+            }
+        }
+}
+
+function deleteRows() {
+    const rows = document.querySelectorAll('.row', '.column');
+    rows.forEach(row => {
+        row.remove();
+    })
+}
+
+createRows()
+
+/*
 for (let i=0; i < numberOfRows; i++) {
     const newRow = document.createElement ('newRow');
     newRow.classList.add('row');
     gridContainer.appendChild(newRow);
-    for (let i=0; i < numberOfColumns; i++) {
+    for (let i=0; i < numberOfRows; i++) {
         const newColumn = document.createElement('newColumn');
         newColumn.classList.add('column');
         newRow.appendChild(newColumn);
@@ -16,3 +62,4 @@ for (let i=0; i < numberOfRows; i++) {
         });
         }
     }
+*/
