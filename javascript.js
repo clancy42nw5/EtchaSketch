1dcab12c4d568fc859931bf4accd831cbf2fe16b
Clancy Wilson <116132142+clancy42nw5@users.noreply.github.com>
2022-11-10 13:15:14 +1100

    Add etch and sketch functionality

--- javascript.js
+++ javascript.js
@@ -1,30 +1,18 @@
-const body = document.querySelector('body');
+const gridContainer = document.querySelector('.gridContainer');
 
-function removeTransition(e) {
-    //if (e.propertyName !== 'transform') return;
-    this.classList.remove('activated');
-}
-
-let numberOfRows = 16
-let numberOfColumns = 16
+let numberOfRows = 100
+let numberOfColumns = 100
 
 for (let i=0; i < numberOfRows; i++) {
     const newRow = document.createElement ('newRow');
     newRow.classList.add('row');
-    body.appendChild(newRow);
+    gridContainer.appendChild(newRow);
     for (let i=0; i < numberOfColumns; i++) {
         const newColumn = document.createElement('newColumn');
         newColumn.classList.add('column');
         newRow.appendChild(newColumn);
         newColumn.addEventListener('mouseover', () => {
             newColumn.classList.add('activated');
-            newColumn.addEventListener('transitionend', removeTransition);
         });
-        newColumn.addEventListener('mouseleave', () => {
-            
-        })
-        newColumn.addEventListener('mousedown', () => {
-            newColumn.classList.add('permanent');
-        })
         }
     }
